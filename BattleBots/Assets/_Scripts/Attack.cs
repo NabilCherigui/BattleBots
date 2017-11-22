82e2a01197940cb988c4f3f5fcbddd80f53b7d7c
NabilCherigui <mrtopgamer@hotmail.com>
2017-11-23 00:54:25 +0100

    Update

---
+++ BattleBots/Assets/_Scripts/Attack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _topHole;
+    [SerializeField] private GameObject _leftHole;
+    [SerializeField] private GameObject _rightHole;
+    [SerializeField] private GameObject _botHole;
+
+}
---
+++ BattleBots/Assets/_Scripts/Hit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour {
+    
+    private void OnTriggerStay(Collider node)
+    {
+        if (node.CompareTag(tag))
+        {
+            if (node.transform.position.x > transform.position.x - (node.transform.localScale.x * 0.5) &&
+                node.transform.position.x < transform.position.x - (node.transform.localScale.x * 0.3))
+            {
+                print("Okay");
+            }
+            else if (node.transform.position.x > transform.position.x - (node.transform.localScale.x * 0.3) &&
+                     node.transform.position.x < transform.position.x - (node.transform.localScale.x * 0.1))
+            {
+                print("Great");
+            }
+            else if (node.transform.position.x > transform.position.x - (node.transform.localScale.x * 0.1) &&
+                     node.transform.position.x < transform.position.x - (node.transform.localScale.x * -0.1))
+            {
+                print("Perfect");
+            }
+            else
+            {
+                print("Bad");   
+            }
+        }
+    }
+}
